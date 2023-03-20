54de573d6f98fb4b131afc086697a0adde58ad6c
urianlinck <urianlinck@gmail.com>
2023-03-20 16:31:28 -0300

    exercicios funcoes resolvidos

---
+++ exercicio3/script.js
@@ -0,0 +1,36 @@
+//a)
+function soma(n1, n2){
+    const soma1 = n1 + n2
+    return soma1
+}
+
+function subtrai(n1, n2){
+    const subtrai1 = n1 - n2
+    return subtrai1
+}
+
+function multiplica(n1, n2){
+    const multi1 = n1 * n2
+    return multi1
+}
+
+function divide(n1, n2){
+    const divide1 = n1 / n2
+    return divide1
+}
+
+//b)
+const n1 = Number(prompt("digite um número"))
+const n2 = Number(prompt("digite outro número"))
+
+//c)
+const soma1 = soma(n1,n2)
+const subtrai1 = subtrai(n1, n2)
+const multiplica1 = multiplica(n1, n2)
+const divide1 = divide(n1, n2)
+
+//d)
+console.log(soma1)
+console.log(divide1)
+console.log(multiplica1)
+console.log(divide1)
